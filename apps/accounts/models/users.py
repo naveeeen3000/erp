957a9582cb7d91a105dba9b5cb4274ad8a690208
Naveen Personal <naveenpersonal@Naveens-MacBook-Pro.local>
2023-03-26 20:04:18 +0530

    added users model

--- apps/accounts/models/users.py
+++ apps/accounts/models/users.py
@@ -1,5 +1,18 @@
-from django import Model
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Users(AbstractUser):
+    user_roles = {
+        "student": "STUDENT",
+        "teacher": "TEACHER"
+    }
+    role = models.CharField(max_length=20,choices=user_roles,default=user_roles["student"])
+    profile_image = models.ImageField()
+    student_id = models.CharField(max_length=20)
+    standard = models.CharField(max_length=20)
+    college_id = models.IntegerField()
+    address = models.TextField()
+    summary = models.TextField()
 
-class Users(Model):
-    
+    def __str__(self) :
+        return self.user.first_name
